2ef9769925613d80f63a989546bb45644d4e988e
gabriel.bauermann <gabriel.bauermann@gmail.com>
2019-03-20 11:37:00 -0300

    Add project files.

---
+++ DemoLibrary/Utilities/SqliteDataAccess.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DemoLibrary.Utilities
+{
+    public class SqliteDataAccess : ISqliteDataAccess
+    {
+        public List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<T>(sql, new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public void SaveData<T>(T person, string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(LoadConnectionString()))
+            {
+                cnn.Execute(sql, person);
+            }
+        }
+
+        public void UpdateData<T>(T person, string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(LoadConnectionString()))
+            {
+                cnn.Execute(sql, person);
+            }
+        }
+
+        private string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
